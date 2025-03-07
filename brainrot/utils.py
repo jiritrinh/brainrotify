327dd4126aa2b289f4a4cfe3057692a91f48b110
Jiri Trinh <trnh.anhtu@gmail.com>
2025-03-07 12:17:48 +0100

    add function to extract text from pdf in utils.py

---
+++ brainrot/utils.py
@@ -0,0 +1,13 @@
+import os
+import pymupdf  
+
+def extract_text(pdf_filename):
+    BASE_DIR = os.path.dirname(__file__)
+    pdf_path = os.path.join(BASE_DIR, "pdfs", pdf_filename)
+    doc = pymupdf.open(pdf_path)  
+    extracted_text = ""
+    for page in doc:  
+        extracted_text += page.get_text() + "\n"  
+    return extracted_text
+
+print(extract_text("cover.pdf"))
