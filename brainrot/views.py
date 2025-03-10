a4239e8ce29d2ee0355b9e7a48861a083ac8d557
Jiri Trinh <trnh.anhtu@gmail.com>
2025-03-11 00:46:09 +0100

    add summarize function from open ai

--- brainrot/views.py
+++ brainrot/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .forms import PDFUploadForm
-from .utils import extract_text
+from .utils import extract_text, summarize
 
 # Create your views here.
 def homepage(request):
-    extracted_text = None
+    final_text = None
     
     if request.method == 'POST':  
         form = PDFUploadForm(request.POST, request.FILES)
@@ -12,9 +12,10 @@ def homepage(request):
             pdf_instance = form.save()
             pdf_path = pdf_instance.file.path
             extracted_text = extract_text(pdf_path)
+            final_text = summarize(extracted_text)
     else:
         form = PDFUploadForm()
-    return render(request, 'brainrot/home.html', {'form': form, 'text': extracted_text})
+    return render(request, 'brainrot/home.html', {'form': form, 'text': final_text})
     
     
     
